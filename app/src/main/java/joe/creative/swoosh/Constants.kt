e1cb92106bf1622e2ba45172084585c0547c4f1d
Joseph Martin <creative.joe007@gmail.com>
2019-12-27 21:13:08 +0100

    * Now you can select a desired league * Added restriction to skill activity if desired league hasn't been clicked

---
+++ app/src/main/java/joe/creative/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package joe.creative.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/joe/creative/swoosh/LeagueActivity.kt
+++ app/src/main/java/joe/creative/swoosh/LeagueActivity.kt
@@ -3,16 +3,47 @@ package joe.creative.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
-
+    var selectedLeague: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
     fun leagueBtnClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java);
-        startActivity(skillActivity)
+        if(!selectedLeague.isEmpty()) {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        }
+        else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    fun onMenClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coEdLeagueBtn.isChecked = false
+
+        selectedLeague = "Men"
+    }
+
+    fun onWomenClicked(view: View) {
+        coEdLeagueBtn.isChecked = false
+        mensLeagueBtn.isChecked = false
+
+        selectedLeague = "Women"
+    }
+
+    fun onCoedClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        mensLeagueBtn.isChecked = false
+
+        selectedLeague = "Coed"
     }
 }
